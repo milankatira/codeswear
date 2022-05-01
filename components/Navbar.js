124b41acee4ea6a8f780739443ba35a7ec14c8b7
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-05-01 18:48:56 +0530

    added checkout sidebar ui

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,87 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import CancelIcon from "@material-ui/icons/Cancel";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 const Navbar = () => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+  const ref = useRef();
   return (
-    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2">
+    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-xl">
       <div className="logo mx-5">
-        <Image width={200} height={40} src="/images/logo.webp" alt="" />
+        <Link href={"/"}>
+          <a>
+            <Image width={200} height={40} src="/images/logo.webp" alt="" />
+          </a>
+        </Link>
       </div>
       <div className="nav">
-        <ul className="flex items-center space-x-2 font-bold md:text-bold ">
-          <Link href={"/"}>
+        <ul className="flex items-center space-x-6 font-bold md:text-md ">
+          <Link href={"/tshirts"}>
             <a>
-              <li>Tshirt</li>
+              <li> Tshirt </li>
             </a>
           </Link>
           <Link href={"/"}>
             <a>
-              <li>Hoodies</li>
+              <li> Hoodies </li>
             </a>
           </Link>
           <Link href={"/"}>
             <a>
-              <li>Stickers</li>
+              <li> Stickers </li>
             </a>
           </Link>
           <Link href={"/"}>
             <a>
-              <li>Mugs</li>
+              <li> Mugs </li>
             </a>
           </Link>
         </ul>
       </div>
+      <div
+        className="cart absolute right-0 top-4 mx-5 cursor-pointer"
+        onClick={toggleCart}
+      >
+        <ShoppingCartIcon className=" text-xl md:text-3xl" />
+      </div>
+      <div
+        ref={ref}
+        className="w-72 h-full hover:shadow-md hover:transition ease-in-out delay-150 sidebar absolute top-0 right-0 bg-purple-100 p-10 transform transition-transform translate-x-full rounded-l-lg duration-800"
+      >
+        <h2 className="font-bold text-xl text-center"> Shopping Cart </h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"
+        >
+          <CancelIcon />
+        </span>
+        <ol className="list-decimal font-semibold">
+          <li>
+            <div className="item flex my-3">
+              <div className="w-2/3 font-semibold"> T-shirt wear the code </div>
+              <div className="w-1/3 font-semibold flex items-center justify-center text-lg">
+                <AddCircleOutlineIcon className="cursor-pointer" />
+                <span className="mx-2">1</span>
+                <RemoveCircleOutlineIcon className="cursor-pointer" />
+              </div>
+            </div>
+          </li>
+        </ol>
 
-      <div className="cart absolute right-0 top-4 mx-5">
-        <button>
-          <ShoppingCartIcon className=" text-xl md:text-3xl" />
+        <button className="flex mt-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg sm:mt-0">
+          <AddShoppingCartIcon className="m-1"/> Checkout
         </button>
       </div>
     </div>
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,9 +1,247 @@
-import React from 'react'
-
+import React from "react";
+import Link from "next/link";
 const Tshirts = () => {
   return (
-    <div>Tshirts</div>
-  )
-}
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>{" "}
+            <Link href={"/werethecode"}>
+              <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                <a className="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
+                    src="https://m.media-amazon.com/images/I/71DNOWgs62L._UL1440_.jpg"
+                  />
+                </a>
+                <div className="mt-4 text-center md:text-left">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    T-shirts
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1">S, M, L, XL, XXL</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Tshirts
+export default Tshirts;
