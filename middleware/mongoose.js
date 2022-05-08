d9a7d5bf07f88b00b02376c435529661004a7ada
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-05-08 14:19:19 +0530

    added product api

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,40 +1,18 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGODB_URI;
-
-if (!MONGO_URL) {
-  throw new Error(
-    "Please define the MONGO_URL environment variable inside .env.local"
+const connectDb = handler => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    return handler(req, res);
+  }
+  await mongoose.connect(
+    process.env.MONGODB_URI
+    // "mongodb://localhost:27017/codeswear"
+    //    {
+    //   // useNewUrlParser: true,
+    //   // useUnifiedTopology: true,
+    // }
   );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDb() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+  return handler(req, res);
+};
 
 export default connectDb;
--- pages/api/getProducts.js
+++ pages/api/getProducts.js
@@ -2,12 +2,15 @@ import Products from "../../model/Product";
 
 import connectDb from "../../middleware/mongoose";
 
-export default async function handler(req, res) {
-  connectDb();
-    try {
-      const products = await Products.find();
-      res.status(200).json(products);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-}
+const handler = async (req, res) => {
+  try {
+    const products = await Products.find();
+    res.status(200).json({ products });
+  } catch (err) {
+
+    console.log(err)
+    res.status(500).json(err);
+  }
+};
+
+export default connectDb(handler);
